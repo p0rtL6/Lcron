6785c50763240c5b60be45568680ab517d7fe267
p0rtL <p0rtl.pue@gmail.com>
2022-01-05 03:05:29 -0500

    l

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,3 @@
 module.exports = {
     transpileDependencies: ["vuetify"],
-    pluginOptions: {
-        electronBuilder: {
-            builderOptions: {
-                squirrelWindows: {
-                    iconUrl:
-                        "https://github.com/p0rtL6/Lcron/blob/master/build/icon.ico?raw=true",
-                    remoteReleases: true,
-                },
-            },
-        },
-    },
 };
